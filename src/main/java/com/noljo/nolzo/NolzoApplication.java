96be4b6738c98129dac7f56fef2b4a72a363c3a3
paka <wjd1357912@gmail.com>
2025-06-04 13:52:49 +0900

    init

---
+++ src/main/java/com/noljo/nolzo/NolzoApplication.java
@@ -0,0 +1,13 @@
+package com.noljo.nolzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/noljo/nolzo/NolzoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noljo.nolzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NolzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
