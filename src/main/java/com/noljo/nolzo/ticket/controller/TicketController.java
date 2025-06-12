d5cb80681052743f82d2116bd58a58edcb3a9a03
hwan2-99 <thh1233123@gmail.com>
2025-06-12 21:04:40 +0900

    feat: 티켓 컨트롤러 종속성 추가

--- src/main/java/com/noljo/nolzo/ticket/controller/TicketController.java
+++ src/main/java/com/noljo/nolzo/ticket/controller/TicketController.java
@@ -1,4 +1,11 @@
 package com.noljo.nolzo.ticket.controller;
 
+import com.noljo.nolzo.ticket.service.TicketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class TicketController {
+    private final TicketService ticketService;
 }
