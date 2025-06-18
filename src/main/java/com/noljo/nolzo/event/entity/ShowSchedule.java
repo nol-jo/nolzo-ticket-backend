5ebd9623baab88cec53c731d1c3029b59f393146
Bogeun Kim <mmmjjjkim@naver.com>
2025-06-18 10:22:40 +0900

    feat: 공연 일장 Embedded 타입 ShowSchedule 생성

---
+++ src/main/java/com/noljo/nolzo/event/entity/ShowSchedule.java
@@ -0,0 +1,22 @@
+package com.noljo.nolzo.event.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShowSchedule {
+
+    @Column
+    private LocalDate showDate;
+
+    @Column
+    private LocalTime showTime;
+}
